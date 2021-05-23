c2fcdf6aadd29a3c453cc0e3f9dfb25906a2cdc2
eedhernandes <edsonfernandohernandes@gmail.com>
2021-05-23 15:45:13 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Figgle;
+using System;
+
+namespace ConversorASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "";
+
+            Console.WriteLine("Insira um texto: ");
+            texto = Console.ReadLine();
+
+            Console.WriteLine(FiggleFonts.Ogre.Render(texto));
+
+            Console.Write("Pressione qualquer tecla para fechar a aplicação...");
+            Console.ReadKey();
+        }
+    }
+}
